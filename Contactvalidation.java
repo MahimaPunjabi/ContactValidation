fd9fd89793d02af257c1fb8a851b0ee2bbd20884
Mahima Punjabi <mahimapunjabi27@gmail.com>
2020-03-02 10:44:40 +0530

    Add files via upload
    A simple contact validation code.

---
+++ Contactvalidation.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CoreJavaAssignments;
+import java.io.*;
+/**
+ *
+ * @author LENOVO
+ */
+public class Contactvalidation 
+{
+    
+    public static void main(String args[]) throws IOException
+    {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter cotact number");
+        String s1=br.readLine();
+        int n=s1.length();
+        
+        if(n==10)
+        {
+            for(int i=0; i<n; i++)
+            {
+                if((s1.charAt(i)<48)||(s1.charAt(i)>57))
+                {
+                    System.out.println("Only digits are alowed");
+                    break;
+                }
+            }
+        }
+        else
+            System.out.println("Only 10 digits are allowed");
+    }
+}
